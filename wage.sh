febba43b4750732e74e1eafd4aae4e9b277bbe6e
yburkul <yburkul123@gmail.com>
2022-02-24 15:50:34 +0530

    Added:Total monthly wage

--- wage.sh
+++ wage.sh
@@ -1,5 +1,15 @@
 echo "Welcome to Employee Wage Computation Program"
 
+work_hr
+{
+emp_rate_per_hr=20
+max_work_hrs=100
+work_day=20
+total_work_hrs=0
+total_work_day=0
+while(( $tatal_work_hrs != $max_work_hrs && $total_work_day != $work_day ))
+do
+
 isPresent=1
 randomCheck=$(( RANDOM%2 ))
 
@@ -14,6 +24,7 @@ fi
 
 emp_time=$(( RANDOM%3 ))
 
+if (( $randomCheck == 1 ))
 case $emp_time in
      1) echo "FullTime"
         work_hrs=8
@@ -25,6 +36,25 @@ case $emp_time in
          work_hrs=16
             ;;
 esac
+echo "Work Hrs" $work_hrs
+total_work_hrs=$(($total_work_hrs + $work_hrs))
+echo "total work hrs is " $total_work_hrs
+
+daily_emp_wage=$(($randomCheck * $work_hrs * $emp_rate_per_hr))
+echo "Daily Employee Wage" $daily_emp_wage
+
+fi
+
+done
+echo "Total work day is: " $total_work_day
+
+total_wage=$(($total_work_hrs * $emp_rate_per_hr))
+echo "Total wage is " $total_wage
+}
+work_hr
+
+echo "Total work hrs is: " $total_work_hrs
+
 
 #Calculate daily emp wage
 
@@ -51,3 +81,7 @@ fi
 work_day=20
 monthly_wage=$(($salary * $work_day))
 echo "Monthly Employee Wage" $monthly_wage
+
+#Total working hours or days is reached for a month
+
+
