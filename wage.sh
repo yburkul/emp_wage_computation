b23bc2388775401b270638a1cad0ee22cfb6331d
yburkul <yburkul123@gmail.com>
2022-02-24 14:15:57 +0530

    Added: Attendance

--- wage.sh
+++ wage.sh
@@ -1,1 +1,11 @@
 echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+
+if (( $randomCheck == 1 ))
+then
+     echo "Present"
+else
+     echo "Absent"
+fi
