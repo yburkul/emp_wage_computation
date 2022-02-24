a15ffa8db80aecc0367a6add23bd11dc14727373
yburkul <yburkul123@gmail.com>
2022-02-24 17:08:40 +0530

    Added:total wage

--- wage.sh
+++ wage.sh
@@ -1,7 +1,5 @@
 echo "Welcome to Employee Wage Computation Program"
 
-work_hr
-{
 emp_rate_per_hr=20
 max_work_hrs=100
 work_day=20
@@ -36,25 +34,11 @@ case $emp_time in
          work_hrs=16
             ;;
 esac
-echo "Work Hrs" $work_hrs
+
 total_work_hrs=$(($total_work_hrs + $work_hrs))
 echo "total work hrs is " $total_work_hrs
-
-daily_emp_wage=$(($randomCheck * $work_hrs * $emp_rate_per_hr))
-echo "Daily Employee Wage" $daily_emp_wage
-
 fi
-
 done
-echo "Total work day is: " $total_work_day
-
-total_wage=$(($total_work_hrs * $emp_rate_per_hr))
-echo "Total wage is " $total_wage
-}
-work_hr
-
-echo "Total work hrs is: " $total_work_hrs
-
 
 #Calculate daily emp wage
 
@@ -82,6 +66,6 @@ work_day=20
 monthly_wage=$(($salary * $work_day))
 echo "Monthly Employee Wage" $monthly_wage
 
-#Total working hours or days is reached for a month
+
 
 
